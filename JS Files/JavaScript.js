17585151fae626489b205b3ef188739070869556
Xinyu Zhang <zhangxinyu0426@gmail.com>
2022-06-12 19:34:20 +0800

    css

--- JS Files/JavaScript.js
+++ JS Files/JavaScript.js
@@ -1,19 +1,10 @@
-function focusproductpic() {
-    document.getElementById('productpic').style.zIndex = 3;
-    document.getElementById('productspec').style.zIndex = 2;
-    document.getElementById('reviews').style.zIndex = 1;
-  }
-  
-  function focusproductspec() {
-    document.getElementById('productspec').style.zIndex = 3;
-    document.getElementById('productpic').style.zIndex = 2;
-    document.getElementById('reviews').style.zIndex = 1;
+
+  function focusinfo() {
+    document.getElementById('info').style.zIndex = 10;
   }
 
-  function focusreviews() {
-    document.getElementById('reviews').style.zIndex = 3;
-    document.getElementById('productpic').style.zIndex = 2;
-    document.getElementById('productspec').style.zIndex = 1;
+  function focusconfirm() {
+    document.getElementById('info').style.zIndex = 10;
   }
 
   
